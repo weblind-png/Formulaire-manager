a41263bc565f1cb758348e2e1c6bd4263cd51aa6
weblind-png <weblind@gmail.com>
2026-08-26 11:24:59 +0200

    Delete transition-guide directory

--- transition-guide/next-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: ce fichier est normalement régénéré automatiquement par `next dev`/`next build`.
-// Il est inclus ici pour que le projet soit valide dès le clone, sans build local préalable.
