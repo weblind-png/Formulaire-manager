55a935765f60f66e8676b4b17e34bc129f452b40
weblind-png <weblind@gmail.com>
2026-08-29 14:54:27 +0200

    Update SiteLogo.tsx

--- components/SiteLogo.tsx
+++ components/SiteLogo.tsx
@@ -1,1 +1,13 @@
+'use client';
 
+export default function SiteLogo() {
+  return (
+    <img
+      src="/logo.png"
+      alt="Logo"
+      onError={(e) => {
+        (e.target as HTMLImageElement).style.display = 'none';
+      }}
+    />
+  );
+}
